2be9bd03a10335d6c0d6ea13984a1be5e06891a5
Vaibhav-gi <vapawar06@gmail.com>
2025-12-30 14:29:51 +0530

    Calculator Task

---
+++ 30_12_25_JavaScript/Hello.js
@@ -0,0 +1,12 @@
+console.log();
+function A() {
+    var x = 10;
+    var y = 20;
+    var sum = x + y;
+    return sum;
+}
+function B() {
+    x = 1;
+   console.log("Value of x in B(): " + x);
+}
+B();
