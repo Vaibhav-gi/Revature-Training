9a70b3587ccbed28fc320abaa23cea4538ad7bf1
Vaibhav-gi <vapawar06@gmail.com>
2026-01-26 19:26:08 +0530

    26-01-26

---
+++ 26_01_26 Maven/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calci calci = new Calci();
+
+        System.out.println(calci.add(10, 5));
+        System.out.println(calci.divide(10, 2));
+    }
+}
